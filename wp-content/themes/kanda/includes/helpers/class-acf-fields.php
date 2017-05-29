4e79b3cd09e92243aa713019ebe997d1d36d4935
Rafik Israelyan <rafik.novembit@gmail.com>
2017-05-29 17:35:34 +0400

    do not update readonly acf fields

--- wp-content/themes/kanda/includes/helpers/class-acf-fields.php
+++ wp-content/themes/kanda/includes/helpers/class-acf-fields.php
@@ -107,6 +107,7 @@ class Kanda_Fields {
      */
     public function make_readonly( $field ) {
         $field['readonly'] = true;
+        $field['disabled'] = true;
 
         return $field;
     }
